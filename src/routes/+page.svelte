a2612950a865a37abeb34604acbbda7ac9e586a7
Robert Newton <robert.newton11@gmail.com>
2023-01-10 18:56:39 -0800

    more responsive tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 	class="hex rotate-[45deg] bg-amber-100 border-amber-100 print:border-white absolute top-0 left-20 z-0"
 />
 
-<div class="p-3 md:p-10 relative z-50">
+<div class="p-3 md:p-10 mr--12 md:m-0 relative z-50">
 	<h1 class="text-6xl md:text-9xl mb-9 py-9 border-b-4 border-green-600">Rob Newton</h1>
 	<div class="flex flex-wrap align-top">
 		<div class="w-full md:w-1/3">
@@ -50,7 +50,7 @@
 			/>
 
 			<SectionHeader title="Skills" icon="skills" />
-			<ul class="ml-10 columns-2 md:columns-3 mb-6">
+			<ul class="ml-10 columns-3 mb-6">
 				{#each skills as skill}
 					<li class="pl-2 mb-1 w-32 bg-gray-100/30 hover:bg-amber-100">{skill}</li>
 				{/each}

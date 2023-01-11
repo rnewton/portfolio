380d1678cc7cd86915c936ab0144b20d81bfec6b
Robert Newton <robert.newton11@gmail.com>
2023-01-10 18:43:42 -0800

    add gh pages deploy

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	export let url: string;
 	export let icon: string;
 	export let label: string;
 </script>
 
 <a href={url} class="font-bold inline-block w-full"
-	><img src={`/${icon}.svg`} alt={`${label} icon`} class="w-6 inline align-top" /><span class="pl-1"
-		>{label}</span
+	><img src={`${base}/${icon}.svg`} alt={`${label} icon`} class="w-6 inline align-top" /><span
+		class="pl-1">{label}</span
 	></a
 >
--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	export let icon: string;
 	export let title: string;
 </script>
 
 <div>
-	<img src={`/${icon}.svg`} class="w-12 inline-block align-middle" alt={`${title} icon`} />
+	<img src={`${base}/${icon}.svg`} class="w-12 inline-block align-middle" alt={`${title} icon`} />
 	<h2 class="text-3xl inline-block align-middle">
 		{title}
 	</h2>
